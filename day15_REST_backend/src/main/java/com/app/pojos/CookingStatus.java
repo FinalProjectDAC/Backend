7adb0611338f654515dec4a262338454700764e8
aniketashinde24 <aniketshinde4971@gmail.com>
2024-08-14 12:34:08 +0530

    hotel manager deliveryboy dao pojos dto and controller added

---
+++ day15_REST_backend/src/main/java/com/app/pojos/CookingStatus.java
@@ -0,0 +1,5 @@
+package com.app.pojos;
+
+public enum CookingStatus {
+	REJECTED,ACCEPTED,PREPARING,READY,NOTYETSTARTED
+}
---
+++ day15_REST_backend/src/main/java/com/app/pojos/hotelType.java
@@ -0,0 +1,5 @@
+package com.app.pojos;
+
+public enum hotelType {
+	VEG,NON_VEG,VEG_NONVEG
+}
